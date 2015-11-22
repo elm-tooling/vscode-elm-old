b034a99d65ec9a339f0d7d4c8ffc195f87ffc968
Sascha Brink <sascha.brink@gmail.com>
2015-11-22 20:07:49 +0100

    Fix dummy test code

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,7 +4,7 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import {assert, test} assert from 'assert';
+import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
