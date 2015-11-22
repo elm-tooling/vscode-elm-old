4fabe7f748a315e5ac6c720092aad7c3f43b888f
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-11-23 00:10:02 +0100

    Add linting function to extension subsriptions

--- src/elmMain.ts
+++ src/elmMain.ts
@@ -2,8 +2,8 @@ import * as vscode from 'vscode';
 import {createDiagnostics} from './elmCheck';
 
 // this method is called when your extension is activated
-export function activate(disposables: vscode.Disposable[]) {
-	vscode.workspace.onDidSaveTextDocument(document => {
+export function activate(ctx: vscode.ExtensionContext) {
+	ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument(document => {
 		createDiagnostics(document);
-	})
+	}))
 }
