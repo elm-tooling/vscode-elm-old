891e63ff019bfcfa27b25cfd4485ab33b753d313
Sascha Brink <sascha.brink@gmail.com>
2015-11-24 21:08:03 +0100

    Reformat code and add types

--- src/elmReactor.ts
+++ src/elmReactor.ts
@@ -4,16 +4,15 @@ import * as cp from 'child_process';
 let reactor: cp.ChildProcess;
 let oc: vscode.OutputChannel = vscode.window.createOutputChannel('Elm Reactor');
 
-
-function startReactor() {
+function startReactor(): void {
   try {
     if (reactor) {
       reactor.kill();
       oc.clear();
     }
-    const config = vscode.workspace.getConfiguration('elm');
-    const host = config.get('reactorHost');
-    const port = config.get('reactorPort');
+    const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('elm');
+    const host: string = <string>config.get('reactorHost');
+    const port: string = <string>config.get('reactorPort');
     reactor = cp.spawn('elm', ['reactor', '-a=' + host, '-p=' + port], { cwd: vscode.workspace.rootPath });
     reactor.stdout.on('data', (data: Buffer) => {
       if (data && data.toString().startsWith('| ') === false) {
@@ -26,29 +25,24 @@ function startReactor() {
       }
     });
     oc.show(vscode.ViewColumn.Three);
-  }
-  catch (e) {
-    console.error("Starting Elm reactor failed", e);
-    vscode.window.showErrorMessage("Starting Elm reactor failed");
+  } catch (e) {
+    console.error('Starting Elm reactor failed', e);
+    vscode.window.showErrorMessage('Starting Elm reactor failed');
   }
 }
 
-function stopReactor()
-{
-  if (reactor)
-  {
+function stopReactor(): void {
+  if (reactor) {
     reactor.kill();
     reactor = null;
-  } 
-  else
-  {
-    vscode.window.showInformationMessage('Elm Reactor not running')
+  } else {
+    vscode.window.showInformationMessage('Elm Reactor not running');
   }
-    
+
 }
 
 export function activateReactor(): vscode.Disposable[] {
   return [
-    vscode.commands.registerCommand('elm.reactorStart', startReactor),  
-    vscode.commands.registerCommand('elm.reactorStop', stopReactor) ]
+    vscode.commands.registerCommand('elm.reactorStart', startReactor),
+    vscode.commands.registerCommand('elm.reactorStop', stopReactor)];
 }
