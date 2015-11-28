4c1118612422609d47c131280e098c6ae67e2387
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-11-28 15:35:40 +0100

    Add make command

---
+++ src/elmMake.ts
@@ -0,0 +1,41 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
+let make: cp.ChildProcess;
+let oc: vscode.OutputChannel = vscode.window.createOutputChannel('Elm Make');
+
+function runMake(editor : vscode.TextEditor) : void {
+  try {
+    if (editor.document.languageId !== 'elm') {
+      return;
+    }
+    if (make) {
+      make.kill();
+      oc.clear();
+    }
+    const file = editor.document.fileName
+    const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('elm');
+    const name: string = <string>config.get('makeOutput');
+    make = cp.spawn('elm-make', [file,'--yes', '--output=' + name], {cwd: vscode.workspace.rootPath})
+    make.stdout.on('data', (data: Buffer) => {
+      if (data) {
+        oc.append(data.toString());
+      }
+    });
+    make.stderr.on('data', (data: Buffer) => {
+      if (data) {
+        oc.append(data.toString());
+      }
+    });
+    oc.show(vscode.ViewColumn.Three);
+  }
+  catch(e){
+     console.error('Running Elm Make failed', e);
+     vscode.window.showErrorMessage('Running Elm Make failed');
+  }
+}
+
+export function activateMake(): vscode.Disposable[] {
+  return [
+    vscode.commands.registerTextEditorCommand('elm.make', runMake)];
+}
