a6f9ce1ea1b25aa5e5dcf657dfdc6f9c419ba0e9
Adam Stevenson <adamstevenson121@gmail.com>
2016-07-05 18:30:41 -0400

    Fix Elm: Make on windows
    Use exec vice spawn on windows platform

--- src/elmMake.ts
+++ src/elmMake.ts
@@ -18,7 +18,13 @@ function runMake(editor : vscode.TextEditor) : void {
     const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('elm');
     const name: string = <string>config.get('makeOutput');
     const cwd: string = utils.detectProjectRoot(file) || vscode.workspace.rootPath;
-    make = cp.spawn('elm-make', [file,'--yes', '--output=' + name], {cwd: cwd})
+    const args = [file, '--yes', '--output=' + name];
+    if (utils.isWindows) {
+      make = cp.exec('elm-make ' + args.join(' '), { cwd: cwd });
+    }
+    else {
+      make = cp.spawn('elm-make', args, { cwd: cwd });
+    }
     make.stdout.on('data', (data: Buffer) => {
       if (data) {
         oc.append(data.toString());
