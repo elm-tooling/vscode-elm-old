27122595dde9b57e510d516063b1e1f07d0af7fb
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2016-10-13 12:10:19 -0400

    updating format for hover fn sig (#82)

--- src/elmInfo.ts
+++ src/elmInfo.ts
@@ -6,12 +6,20 @@ export class ElmHoverProvider implements vscode.HoverProvider {
     return oracle.GetOracleResults(document, position)
       .then((result) => {
         if (result.length > 0) {
-            let text = result[0].signature + '\n\n' + result[0].comment;
-            let hover = new vscode.Hover(text);
-            return hover;
+          let text =  this.formatSig(result[0].signature) + '\n\n' + result[0].comment;
+          let hover = new vscode.Hover(text);
+          return hover;
         }
         else {
           return null;
         }})
+  }
+  private formatSig(signature: String): String {
+    return '~~~\n' 
+      + signature
+        .replace(/\{/g, '  {')        //spaces before open brace
+        .replace(/\s?,/g, '\n  ,')    //newlines + spaces before comma
+        .replace(/\}\s?/g, '\n  }\n') //newline + spaces before close brace + newline after  
+      + '\n~~~';
   }
 }
