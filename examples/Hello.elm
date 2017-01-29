9a771fab81ae81bc3e0eec92bbc5005da4d34bd9
Joe Andaverde <joe.andaverde@gmail.com>
2017-01-28 19:11:37 -0600

    Updated elm example

--- examples/Hello.elm
+++ examples/Hello.elm
@@ -1,5 +1,50 @@
-import Graphics.Element exposing (..)
+module Main exposing (..)
 
-main : Element
+import Html exposing (Html, div, button, text)
+import Html.Events exposing (onClick)
+
+
 main =
-  show "Hello, World!"
+    Html.program
+        { init = ( initialModel, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type alias Model =
+    Int
+
+
+initialModel : Model
+initialModel =
+    0
+
+
+type Msg
+    = Increment
+    | Decrement
+    | NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, Cmd.none )
+
+        Decrement ->
+            ( model - 1, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
