b7019990c5adf28690289aee3a59b56e36e44a1a
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-05-09 23:51:46 +0200

    Handle null in autocomplete

--- src/elmAutocomplete.ts
+++ src/elmAutocomplete.ts
@@ -7,12 +7,15 @@ export class ElmCompletionProvider implements vscode.CompletionItemProvider {
     let currentWord: string = document.getText(wordRange);
     return oracle.GetOracleResults(document, position, oracle.OracleAction.IsAutocomplete)
       .then((result) => {
+        if (result == null) {
+          return [];
+        }
         var r = result.map((v, i, arr) => {
           var ci: vscode.CompletionItem = new vscode.CompletionItem(v.fullName.trim());
           ci.kind = (v.kind !== undefined ? v.kind : 0);
           if (currentWord.substr(-1) !== '.') {
             ci.insertText = v.name.startsWith(currentWord) ? v.name : v.fullName;
-          }  
+          }
           ci.detail = v.signature;
           ci.documentation = v.comment;
           if (currentWord.substr(-1) === '.') {
@@ -20,7 +23,7 @@ export class ElmCompletionProvider implements vscode.CompletionItemProvider {
               range: new vscode.Range(position, position),
               newText: v.fullName.trim()
             };
-          }  
+          }
 
           return ci;
         });
