0f50dd7323d3c40bb275b8d160303e68fb5507be
Asger Nielsen <ani@conexio.dk>
2018-02-26 20:07:27 +0100

    print functions

--- src/elmFunctionHeader.ts
+++ src/elmFunctionHeader.ts
@@ -5,6 +5,28 @@ import {
   CodeLens,
 } from 'vscode';
 
+//
+// Module List exposing (map)
+//
+//
+// 0 references | exposed
+// map : (a -> b) -> List a -> List b
+// map op list =
+//     case list of
+//         [] ->
+//             []
+//         head :: tail ->
+//             op head :: map op tail
+//
+
+const printReferences = function (count: number): string {
+  return `${count} reference${Math.abs(count) === 1 ? '' : 's'}`;
+};
+
+const printExposure = function (exposed: boolean): string {
+  return exposed ? 'exposed' : 'hidden';
+};
+
 export class ElmFunctionHeaderProvider implements CodeLensProvider {
   provideCodeLenses(document: TextDocument, token: CancellationToken): CodeLens[] | Thenable<CodeLens[]> {
     throw new Error('Method not implemented.');
