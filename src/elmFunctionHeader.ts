5c8d9a3901deb51d28886971b9c5b90a4441f517
Asger Nielsen <ani@conexio.dk>
2018-02-27 16:59:56 +0100

    get something on-screen

--- src/elmFunctionHeader.ts
+++ src/elmFunctionHeader.ts
@@ -3,6 +3,10 @@ import {
   CancellationToken,
   TextDocument,
   CodeLens,
+  Range,
+  Position,
+  Command,
+  TextLine,
 } from 'vscode';
 
 //
@@ -27,11 +31,48 @@ const printExposure = function (exposed: boolean): string {
   return exposed ? 'exposed' : 'hidden';
 };
 
+const isDefinition = function (line: TextLine): boolean {
+  return !line.isEmptyOrWhitespace
+    && line.text.indexOf(' ')
+    && !line.text.startsWith('module ')
+    && !line.text.startsWith('import ')
+    && !line.text.startsWith('type ');
+};
+
+const firstSymbol = function (line: TextLine): string {
+  return line.text.substring(0, line.text.indexOf(' '));
+};
+
 export class ElmFunctionHeaderProvider implements CodeLensProvider {
   provideCodeLenses(document: TextDocument, token: CancellationToken): CodeLens[] | Thenable<CodeLens[]> {
-    throw new Error('Method not implemented.');
+
+    const exposed: string[] = [];
+    const definitions: Map<string, number> = new Map();
+    let line: TextLine;
+    let symbol: string;
+
+    for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+      line = document.lineAt(lineNumber);
+
+      if (isDefinition(line)) {
+        symbol = firstSymbol(line);
+        if (!definitions.has(symbol)) {
+          definitions.set(symbol, lineNumber);
+        }
+      }
+    }
+
+    return Array.from(definitions.entries()).reduce((list, position) =>
+      list.concat([new CodeLens(new Range(new Position(position['1'], 0), new Position(position['1'], 0)), {
+        command: '',
+        title: printReferences(0),
+      }), new CodeLens(new Range(new Position(position['1'], 0), new Position(position['1'], 0)), {
+        command: '',
+        title: printExposure(false),
+      })]), []);
   }
+
   resolveCodeLens?(codeLens: CodeLens, token: CancellationToken): CodeLens | Thenable<CodeLens> {
-    throw new Error('Method not implemented.');
+    return codeLens;
   }
 }
