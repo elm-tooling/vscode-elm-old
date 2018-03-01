aeeba573f14532c11130d108ed07990fb575ba5e
Asger Nielsen <ani@conexio.dk>
2018-03-01 06:50:09 +0100

    find function definitions

--- src/elmFunctionHeader.ts
+++ src/elmFunctionHeader.ts
@@ -31,7 +31,7 @@ const printExposure = function (exposed: boolean): string {
   return exposed ? 'exposed' : 'hidden';
 };
 
-const isDefinition = function (line: TextLine): boolean {
+const hasDefinition = function (line: TextLine): boolean {
   return !line.isEmptyOrWhitespace
     && line.text.indexOf(' ')
     && !line.text.startsWith('module ')
@@ -43,33 +43,44 @@ const firstSymbol = function (line: TextLine): string {
   return line.text.substring(0, line.text.indexOf(' '));
 };
 
+const findDefinitions = function (document: TextDocument): Map<string, number> {
+  const definitions: Map<string, number> = new Map();
+  let line: TextLine;
+  let symbol: string;
+
+  for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+    line = document.lineAt(lineNumber);
+
+    if (hasDefinition(line)) {
+      symbol = firstSymbol(line);
+      if (!definitions.has(symbol)) {
+        definitions.set(symbol, lineNumber);
+      }
+    }
+  }
+
+  return definitions;
+};
+
 export class ElmFunctionHeaderProvider implements CodeLensProvider {
   provideCodeLenses(document: TextDocument, token: CancellationToken): CodeLens[] | Thenable<CodeLens[]> {
 
     const exposed: string[] = [];
-    const definitions: Map<string, number> = new Map();
-    let line: TextLine;
-    let symbol: string;
+    const definitions: Map<string, number> = findDefinitions(document);
 
-    for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
-      line = document.lineAt(lineNumber);
+    return Array.from(definitions.entries()).reduce((list, lineNumber) => {
 
-      if (isDefinition(line)) {
-        symbol = firstSymbol(line);
-        if (!definitions.has(symbol)) {
-          definitions.set(symbol, lineNumber);
-        }
-      }
-    }
+      let position = new Position(lineNumber['1'], 0);
+      let range = new Range(position, position);
 
-    return Array.from(definitions.entries()).reduce((list, position) =>
-      list.concat([new CodeLens(new Range(new Position(position['1'], 0), new Position(position['1'], 0)), {
+      return list.concat([new CodeLens(range, {
         command: '',
         title: printReferences(0),
-      }), new CodeLens(new Range(new Position(position['1'], 0), new Position(position['1'], 0)), {
+      }), new CodeLens(range, {
         command: '',
-        title: printExposure(false),
-      })]), []);
+        title: printExposure(exposed.indexOf(lineNumber['0']) !== -1),
+      })]);
+    }, []);
   }
 
   resolveCodeLens?(codeLens: CodeLens, token: CancellationToken): CodeLens | Thenable<CodeLens> {
