3fb9bc988a6de92cd279cae4170be79477ed6001
Håkon Rossebø <hakonrossebo@gmail.com>
2018-08-28 19:43:23 +0200

    Fixes to omit Elm Oracle for 0.19 and read elm.json (#254)

--- src/elmOracle.ts
+++ src/elmOracle.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as userProject from './elmUserProject';
 import * as vscode from 'vscode';
 
-import { detectProjectRoot, pluginPath } from './elmUtils';
+import { detectProjectRoot, pluginPath, detectProjectRootAndElmVersion } from './elmUtils';
 
 export interface IOracleResult {
   name: string;
@@ -40,7 +40,7 @@ export function GetOracleResults(
   return new Promise((resolve: Function, reject: Function) => {
     let p: cp.ChildProcess;
     let filename: string = document.fileName;
-    let cwd = detectProjectRoot(document.fileName) || vscode.workspace.rootPath;
+    let [cwd, elmVersion] = detectProjectRootAndElmVersion(document.fileName, vscode.workspace.rootPath) || vscode.workspace.rootPath;
     let fn = path.relative(cwd, filename);
     let wordAtPosition = document.getWordRangeAtPosition(position);
     if (!wordAtPosition) {
@@ -48,27 +48,41 @@ export function GetOracleResults(
     }
     let currentWord: string = document.getText(wordAtPosition);
 
-    p = cp.execFile(
-      'node',
-      [oraclePath, fn, currentWord],
-      { cwd: cwd },
-      (err: Error, stdout: string, stderr: string) => {
-        try {
-          if (err) {
-            return resolve(null);
-          }
+    if (elmVersion === '0.18') {
 
-          const result: IOracleResult[] = [
-            ...JSON.parse(stdout),
-            ...(config['userProjectIntellisense']
-              ? userProject.userProject(document, position, currentWord, action)
-              : []),
-          ];
-          resolve(result);
-        } catch (e) {
-          reject(e);
-        }
-      },
-    );
+      p = cp.execFile(
+        'node',
+        [oraclePath, fn, currentWord],
+        { cwd: cwd },
+        (err: Error, stdout: string, stderr: string) => {
+          try {
+            if (err) {
+              return resolve(null);
+            }
+
+            const result: IOracleResult[] = [
+              ...JSON.parse(stdout),
+              ...(config['userProjectIntellisense']
+                ? userProject.userProject(document, position, currentWord, action)
+                : []),
+            ];
+            resolve(result);
+          } catch (e) {
+            reject(e);
+          }
+        },
+      );
+    } else {
+      try {
+        const result: IOracleResult[] = [
+          ...(config['userProjectIntellisense']
+            ? userProject.userProject(document, position, currentWord, action)
+            : []),
+        ];
+        resolve(result);
+      } catch (e) {
+        reject(e);
+      }
+    }
   });
 }
