96220c28c493245ea21ee4e86167225e0dad24c9
Joe Andaverde <joe.andaverde@smrxt.com>
2018-11-21 21:14:37 -0600

    Reinitialize project manager when changing workspace folders.

--- src/elmProjectManager.ts
+++ src/elmProjectManager.ts
@@ -2,8 +2,16 @@ import * as vscode from 'vscode';
 import { ElmProjectManager } from 'elm-project-inspect';
 import * as _ from 'lodash';
 
-export const getGlobalProjectManager = _.memoize(() => {
+const initializeElmProjectManager = () => {
   return new ElmProjectManager(
     (vscode.workspace.workspaceFolders || []).map(x => x.uri.fsPath),
   );
+};
+
+let elmProjectManager = initializeElmProjectManager();
+
+vscode.workspace.onDidChangeWorkspaceFolders(() => {
+  elmProjectManager = initializeElmProjectManager();
 });
+
+export const getGlobalProjectManager = () => elmProjectManager;
